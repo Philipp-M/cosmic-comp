080b23dd8805e5fb27b3219df1af3c1054367cbc
Philipp Mildenberger <philipp@mildenberger.me>
2025-07-19 01:00:09 +0200

    Fix?

--- src/backend/kms/device.rs
+++ src/backend/kms/device.rs
@@ -264,7 +264,7 @@ impl State {
                 gbm.clone(),
                 GbmBufferFlags::RENDERING | GbmBufferFlags::SCANOUT,
             ),
-            GbmFramebufferExporter::new(gbm.clone(), Some(drm_node)),
+            GbmFramebufferExporter::new(gbm.clone(), Some(drm_node).into()),
             Some(gbm.clone()),
             [
                 Fourcc::Abgr2101010,
